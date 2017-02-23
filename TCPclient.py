e67942582078890a542390758ab2ef0bef9c51d7
Richard-Lynch <rich753@hotmail.com>
2017-02-23 00:11:08 +0000

    AHHH

--- TCPclient.py
+++ TCPclient.py
@@ -1,106 +1,163 @@
-from socket import *    
+from socket import *
 import thread
+import time
+from random import randint
 import crc16
 import json
 
 serverName = 'localhost'
 serverPort = 12014
-clientSocket = socket(AF_INET, SOCK_STREAM) #create client socket
-clientSocket.connect((serverName,serverPort)) #connect to the server
 print 'Checking crc test', crc16.crc16xmodem('Hello, World!')
+clientSocket = socket(AF_INET, SOCK_STREAM)  # create client socket
+clientSocket.connect((serverName, serverPort))  # connect to the server
 print 'Client has successfully connected to server'
+# thread.start_new_thread(onRec, (clientSocket, addr))
+# thread.start_new_thread(writeFromBuffer, (clientSocket, addr))
 loop = True
 next_ack = 0
 min_buff = 0
-max_buff = 0
+buff_index = 0
 buff_size = 5
+buff = []
 while loop:
-    sentence = clientSocket.recv(1024)  #receive packets
-    while len(sentence) > 0: # if there are any chars left in the string
+    sentence = clientSocket.recv(1024)  # receive packets
+    while len(sentence) > 0:  # if there are any chars left in the string
         length = len(sentence)
-        i = 0                # position in the string to start the frame
-        while sentence[i] != '{':   #look for a '{'
-            i += 1
-            if(i >= length):        #if the string has been exhausted
+        i = 0                # position in the string to start looking for the frame
+        while sentence[i] != '{':  # look for a '{'
+            if(i >= length):  # if the string has been exhausted
                 break
+            else:
+                i += 1
         j = i                 # where to start looking for the end of the frame
         quote = False
-        while sentence[j] != '}' & quote == False:  #if you've found a closing bracket and you're not inside a quote
-            if sentence[j] == '"':      #if inside a quote toggle quote bool
+        # if you've found a closing bracket and you're not inside a quote
+        while sentence[j] != '}' & quote == False:
+            if sentence[j] == '"':  # if inside a quote toggle quote bool
                 quote = not quote
-            j += 1                  #iterate
+            
             if(j >= length):    # if string has been exhausted
                 break
-        j += 1          #incldue the closing }
-        buff[max_buff] = Frame()    #add to the buffer
-        buff[max_buff].readFrame(sentence[i:j]) #
-        max_buff += 1
-        sentence = sentence[j:]
+            else:
+                j += 1  # iterate
 
+        j += 1  # incldue the closing }
+        buff[buff_index] = Frame()  # add to the buffer
+        buff[buff_index].readFrame(sentence[i:j])   #convert the string to the data
+        buff_index += 1                            
+        sentence = sentence[j:]                     #shorten the string
+    # after reading all the packets received
 
-    
+    while i < buff_index:
+        if buff[i].data['SeqNum'] = next_ack:  # if the frame being checked is the next to be acked
+            buff[i].ack()  # then ack
+            next_ack += 1  # increment the next ack
+            buff.pop(i)  # then pop the frame off the stack
+            buff_index -= 1  # reduce the number of items in the buffer
+            i = 0  # start you search of the buffer again
 
-    if sentence[0] == '1':  
-        # calc checksum 
-        print sentence[1], ' Ack? ', sentence[-1:] # print the checksum result
-         
-        if sentence[-1:] != '2': # if checksum doesnt match
-            ack = 'Nack - ' + sentence[1] # send Nack with frame number
+        # if the frame being checked should already have been acked
+        elif buff[i].data['SeqNum'] < next_ack:
+            buff[i].ack()  # then ack
+            buff.pop(i)  # then pop the frame off the stack
+            buff_index -= 1  # reduce the number of items in the buffer
+            i = 0  # start you search of the buffer again
+
+        else:
+            i += 1  # otherwise check the next item in the buffer
+
+    if sentence[0] == '1':
+        # calc checksum
+        print sentence[1], ' Ack? ', sentence[-1:]  # print the checksum result
+
+        if sentence[-1:] != '2':  # if checksum doesnt match
+            ack = 'Nack - ' + sentence[1]  # send Nack with frame number
             clientSocket.send(ack)
         else:                    # if the checksum doesnt match
-            ack = 'Ack - ' + sentence[1]    #send Ack with frame number
+            ack = 'Ack - ' + sentence[1]  # send Ack with frame number
             clientSocket.send(ack)
-            
-    elif sentence[0] == '2':    #if at the end of the data stream
-        ack = 'Ack - ' + sentence[1]    #ack the last packet
-        clientSocket.send(ack)  
-        loop = False #leave the loop
-    print serverName, ' >> ', sentence[2:-1] #print the message from the server
-clientSocket.close()    #when we leave the loop close the socket
+
+    elif sentence[0] == '2':  # if at the end of the data stream
+        ack = 'Ack - ' + sentence[1]  # ack the last packet
+        clientSocket.send(ack)
+        loop = False  # leave the loop
+    # print the message from the server
+    print serverName, ' >> ', sentence[2:-1]
+clientSocket.close()  # when we leave the loop close the socket
+
+
+class Stack:
+
+    def __init__(self):  # any intneral methods needs to start with self or it will be complelty public
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def pop(self, position):
+        if position != -1:
+            return self.items.pop(position)
+
+    def push(self, item):
+        self.items.insert(0, item)
+
+    def flush(self):
+        self.items = []
+
+    def length(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
 
 
 class Frame:
-    def __init__(self):     #any intneral methods needs to start with self or it will be complelty public
+
+    def __init__(self):  # any intneral methods needs to start with self or it will be complelty public
         # self.chars = []     #0=start 1=SeqNum 2=PayloadLength 3=PayLoad 4=Ack/Nack 5=Checksum
         # self.chars[0] = 's'
         # self.minLength = 13
         self.data = {
-        'start': 's'
+            'start': 's'
         }
-        
+
     def __str__(self):
         return json.dumps(self.data, sort_keys=True, indent=4, separators=(',', ': '))
 
     def setSeq(self, SeqNum):
-        #input SeqNum here, with min length of 4
+        # input SeqNum here, with min length of 4
         # self.data['SeqNum'] = "{0:0>4}".format(SeqNum)
         self.data['SeqNum'] = SeqNum
 
     def addPayload(self, payload):
         self.data['PayLoad'] = payload
-    
+
     def calcLength(self):
-        #payload length, min length 4
-        # self.data['PayloadLength'] = "{0:0>4}".format(len(self.data['PayLoad'])) #probs need to typecase this
+        # payload length, min length 4
+        # self.data['PayloadLength'] =
+        # "{0:0>4}".format(len(self.data['PayLoad'])) #probs need to typecase
+        # this
         self.data['PayloadLength'] = len(self.data['PayLoad'])
-    
+
     def calcSum(self):
-        #calc checksum here, with min length of 4
+        # calc checksum here, with min length of 4
         self.checkSum = 1
         # self.data['checksum'] = "{0:0>4}".format(checkSum)
         self.data['checksum'] = self.checkSum
-    
+
     def prepSend(self):
         self.data_string = json.dumps(self.data)
         return self.data_string
-    
+
     def readFrame(self, data_string):
         self.data_string = data_string
-        data = json.loads(self.data_string)
+        self.data = json.loads(self.data_string)
 
     def compCheckSum(self):
         # calc new checksum
-        self.newCheckSum = 1
+        self.newCheckSum = self.data['checksum']
+        self.data['checksum'] = 0
+        self.checksum = crc16.crc16xmodem(json.dumps(self.data))
         if self.checkSum == self.newCheckSum:
             return True
         else:
@@ -127,4 +184,3 @@ class Frame:
         while chars[0] != 's':
             chars = chars[1:]   # chop off the first char
     '''
-
--- TCPserver.py
+++ TCPserver.py
@@ -27,8 +27,11 @@ def on_con(clientSocket, address):
                 clientSocket.send(c)
                 break
             print "Read two characters:", c
-            list.insert(loop, c)
-            print 'loop: ', str(loop), ' list[loop]: ', list[loop]
+            list1.insert(loop, c)
+            print 'loop: ', str(loop), ' list1[loop]: ', list1[loop]
+            temp = new Frame()
+            temp.setSeq(loop)
+            temp.addPayload(list1[loop])
             # sentence = clientSocket.recv(1024)
             # print address, ' >> ', sentence
             # msg = raw_input('SERVER >> ')
@@ -56,8 +59,8 @@ def on_ack(clientSocket, address):
                 print 'ack[-1:]', ack[-1:]
                 ind = int(ack[-1:])
                 print 'ind: ', ind
-                print 'list[ind]: ', list[ind]
-                remsg = '1' + str(ind) + list[ind] + str(grem())
+                print 'list1[ind]: ', list1[ind]
+                remsg = '1' + str(ind) + list1[ind] + str(grem())
                 clientSocket.send(remsg)
             # msg = raw_input('SERVER >> ')
             # capitalizedSentence = sentence.upper()
@@ -86,15 +89,15 @@ def on_ack(clientSocket, address):
 serverName = 'localhost'
 serverPort = 12014
 fileName = 'input.txt'
-list = ['', '', '', '', '', '', '', '']
+list1 = ['', '', '', '', '', '', '', '']
 c = ''
 # create a NET, streaming socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # bind the socket to the host and port
 serverSocket.bind((serverName, serverPort))
-# become a server socket by listening - we will let 1 connection request
+# become a server socket by list1ening - we will let 1 connection request
 # occur before we refuse connections
-serverSocket.listen(1)
+serverSocket.list1en(1)
 print 'the server is ready to receive'
 
 while True:
@@ -104,3 +107,57 @@ while True:
     thread.start_new_thread(on_con, (clientSocket, addr))
     thread.start_new_thread(on_ack, (clientSocket, addr))
 serverSocket.close()
+
+class Frame:
+
+    def __init__(self):  # any intneral methods needs to start with self or it will be complelty public
+        # self.chars = []     #0=start 1=SeqNum 2=PayloadLength 3=PayLoad 4=Ack/Nack 5=Checksum
+        # self.chars[0] = 's'
+        # self.minLength = 13
+        self.data = {
+            'start': 's'
+        }
+
+    def __str__(self):
+        return json.dumps(self.data, sort_keys=True, indent=4, separators=(',', ': '))
+
+    def setSeq(self, SeqNum):
+        # input SeqNum here, with min length of 4
+        # self.data['SeqNum'] = "{0:0>4}".format(SeqNum)
+        self.data['SeqNum'] = SeqNum
+
+    def addPayload(self, payload):
+        self.data['PayLoad'] = payload
+
+    def calcLength(self):
+        # payload length, min length 4
+        # self.data['PayloadLength'] =
+        # "{0:0>4}".format(len(self.data['PayLoad'])) #probs need to typecase
+        # this
+        self.data['PayloadLength'] = len(self.data['PayLoad'])
+
+    def calcSum(self):
+        # calc checksum here, with min length of 4
+        self.data['checksum'] = 0
+        self.checksum = crc16.crc16xmodem(json.dump(self.data))
+        self.data['checksum'] = self.checkSum
+
+    def prepSend(self):
+        self.calcLength()
+        self.calcSum()
+        self.data_string = json.dumps(self.data)
+        return self.data_string
+
+    def readFrame(self, data_string):
+        self.data_string = data_string
+        self.data = json.loads(self.data_string)
+
+    def compCheckSum(self):
+        # calc new checksum
+        self.newCheckSum = self.data['checksum']
+        self.data['checksum'] = 0
+        self.checksum = crc16.crc16xmodem(json.dumps(self.data))
+        if self.checkSum == self.newCheckSum:
+            return True
+        else:
+            return False
